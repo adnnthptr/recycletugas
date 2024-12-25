c7429c9a8f8fa9aa890297a9fbe4f577461eecfb
adnnthptr <adeanantaputra863@gmail.com>
2024-12-25 20:44:52 +0700

    Tugas2

--- app/Http/Controllers/PelangganController.php
+++ app/Http/Controllers/PelangganController.php
@@ -29,9 +29,26 @@ class PelangganController extends Controller
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
-    {
-        //
-    }
+{
+    $request->validate([
+        'kode_pelanggan' => 'required|unique:pelanggans,kode_pelanggan',
+        'nama_pelanggan' => 'required',
+        'kendaraan' => 'required',
+        'no_hp' => 'required',
+        'alamat' => 'required'
+    ]);
+
+    $pelanggan = new \App\Models\Pelanggan();
+    $pelanggan->kode_pelanggan = $request->kode_pelanggan;
+    $pelanggan->nama_pelanggan = $request->nama_pelanggan;
+    $pelanggan->kendaraan = $request->kendaraan;
+    $pelanggan->no_hp = $request->no_hp;
+    $pelanggan->alamat = $request->alamat;
+    $pelanggan->save();
+
+    return back()->with('pesan','Data sudah Disimpan');
+}
+
 
     /**
      * Display the specified resource.
@@ -64,13 +81,13 @@ class PelangganController extends Controller
             'alamat' => 'required'
         ]);
     
-        $manager = \App\Models\Manager::findOrFail($id);
-        $manager->kode_pelanggan = $request->kode_pelanggan;
-        $manager->nama_pelanggan = $request->nama_pelanggan;
-        $manager->kendaraan = $request->kendaraan;
-        $manager->no_hp = $request->no_hp;
-        $manager->alamat = $request->alamat;
-        $manager->save();
+        $pelanggan = \App\Models\Pelanggan::findOrFail($id);
+        $pelanggan->kode_pelanggan = $request->kode_pelanggan;
+        $pelanggan->nama_pelanggan = $request->nama_pelanggan;
+        $pelanggan->kendaraan = $request->kendaraan;
+        $pelanggan->no_hp = $request->no_hp;
+        $pelanggan->alamat = $request->alamat;
+        $pelanggan->save();
         return redirect('/pelanggan')->with('pesan', 'Data sudah Diupdate');
     }
 
@@ -81,4 +98,12 @@ class PelangganController extends Controller
     {
         //
     }
+
+    public function laporan()
+    {
+    $data['judul'] = 'Laporan Data Pelanggan';
+    $data['pelanggan'] = \App\Models\Pelanggan::all();
+    return view('pelanggan_laporan', $data);
+    }
+
 }
